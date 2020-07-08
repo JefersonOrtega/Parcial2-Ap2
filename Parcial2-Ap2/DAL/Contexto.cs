db9ab0ad005767e4d2740d1b71eb79c970418580
Jeferson Raul Ortega Brito <jeferson_ortega@ucne.edu.do>
2020-07-08 17:28:30 -0300

    Configurando Proyecto

---
+++ Parcial2-Ap2/DAL/Contexto.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial2_Ap2.DAL
+{
+    public class Contexto : DbContext
+    {
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source=DATA/SegundoParcialAP2.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<>().HasData(new
+            {
+
+            });
+        }
+        
+
+        
+    }
+}
